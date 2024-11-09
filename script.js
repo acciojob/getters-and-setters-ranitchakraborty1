d1a88ee70f3ff9a8a2b21777600f32abb1f806e1
ranitchakraborty1 <162864925+ranitchakraborty1@users.noreply.github.com>
2024-11-09 09:59:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 //complete this code
 class Person {
-	const person = new Person("John", 25);
-	console.log(person.name);
-	person.age = 30;
-    console.log(person.age);
+	constructor(name,age){
+		this.name=name;
+		this.age=age;
+	}
+	get name(){
+		return this.name;
+	}
+	set age(){
+		this.age=age;
+	}
+	get age(){
+		return this.age;
+	}
 }
 
 class Student extends Person {
-	const student = new Student("Alice", 22);
-	student.study();
+
 }
 
 class Teacher extends Person {
-	const teacher = new Teacher("Bob", 40);
-teacher.teach();
+	
 }
 
 // Do not change the code below this line
