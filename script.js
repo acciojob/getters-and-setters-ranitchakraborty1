7411d5bba137893cda3eba7710e962b4e90ecd8a
ranitchakraborty1 <162864925+ranitchakraborty1@users.noreply.github.com>
2024-11-09 09:50:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 //complete this code
-class Person {}
+class Person {
+	const person = new Person("John", 25);
+	console.log(person.name);
+	person.age = 30;
+    console.log(person.age);
+}
 
-class Student extends Person {}
+class Student extends Person {
+	const student = new Student("Alice", 22);
+	student.study();
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	const teacher = new Teacher("Bob", 40);
+teacher.teach();
+}
 
 // Do not change the code below this line
 window.Person = Person;
